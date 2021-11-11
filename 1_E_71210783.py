ab8adc1e4e0da91e131b2ec24df7bd154dbd9853
liana chienti <liana.chienti@ti.ukdw.ac.id>
2021-11-12 01:28:21 +0700

    Soal 1

---
+++ 1_E_71210783.py
@@ -0,0 +1,6 @@
+#Menghitung Luas Trapesium
+aa = int(input("Masukan Alas Atas :"))
+ab = int(input("Menambahkan Alas Bawah :"))
+t = int(input("Masukan Tinggi :"))
+L = 0.5 * (aa + ab) * t
+print ("Luas Trapesium adalah :", L)

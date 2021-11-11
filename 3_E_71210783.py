eb986568df48b9d17dcfbef73a61e1e37b184ecd
liana chienti <liana.chienti@ti.ukdw.ac.id>
2021-11-12 01:32:59 +0700

    Soal 3

---
+++ 3_E_71210783.py
@@ -0,0 +1,10 @@
+#Catatan program Dani
+a = input("Masukan daftar belanja 1 : ")
+b = input("Masukan daftar belanja 2 : ")
+c = input("Tambahkan data ke daftar belanja 1 : ")
+d = input("Tambahkan data ke daftar belanja 2 : ")
+
+print("Daftar belanja 1 adalah",a + ",", end="")
+print(c)
+print("Daftar belanja 2 adalah",b + ",", end="")
+print(d)
